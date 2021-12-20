0f7ecc936b0564cd055b78921326de92bd2861a8
taifu yuan <515789026@qq.com>
2021-12-20 20:31:23 +0800

    add script with new branch
    (cherry picked from commit a14325cc8b150af9504f754b08876e641992d7d3)

---
+++ scripts/make_new_branch.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) BABEC. All rights reserved.
+# Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#set -x
+BRANCH=develop
+NEW_BRANCH=v2.2.0_alpha_qc
+PRE_LOG="【log】 "
+
+
+projects=("pb-go"
+  "common"
+  "protocol"
+  "logger"
+  "utils"
+  "localconf"
+  "chainconf"
+  "txpool-single"
+  "txpool-batch"
+  "store-leveldb"
+  "store-badgerdb"
+  "store-sqldb"
+  "store"
+  "vm-native"
+  "vm"
+  "vm-wasmer"
+  "vm-gasm"
+  "vm-wxvm"
+  "vm-evm"
+  "vm-docker-go"
+  "raftwal"
+  "libp2p-core"
+  "net-common"
+  "libp2p-pubsub"
+  "net-libp2p"
+  "net-liquid"
+  "consensus-utils"
+  "consensus-dpos"
+  "consensus-maxbft"
+  "consensus-raft"
+  "consensus-solo"
+  "consensus-tbft"
+  "chainmaker-go"
+)
+BRANCH=develop
+BRANCH_NEW=v2.2.0_alpha_qc
+
+cd ../..
+for project in ${projects[*]}; do
+  #如果文件夹不存在，则克隆
+  if [ ! -d ${project} ]; then
+    git clone chainmaker.org/chainmaker/${project}.git
+  fi
+
+  cd $project
+  echo "$PRE_LOG $(pwd)"
+  echo "$PRE_LOG cd $project && git stash && git checkout $BRANCH && git push origin $BRANCH_NEW"
+  git fetch --all
+  if git rev-parse --verify remotes/origin/${BRANCH_NEW}; then
+    echo "$PRE_LOG do nothing for branch $project $BRANCH_NEW then the branch already exist"
+    cd ..
+    continue
+  fi
+
+  git stash
+  git checkout $BRANCH
+  git reset --hard
+  git pull
+  git checkout -b $BRANCH_NEW
+  sed -i "s%VERSION=.*%VERSION=${BRANCH_NEW}%g" Makefile
+  make gomod
+
+  git status
+  git add Makefile go.mod go.sum
+  git commit -m "auto script/checkout_new_branch.sh make new branch"
+  git push origin $BRANCH_NEW
+  cd ..
+  echo "$PRE_LOG push $project $BRANCH_NEW finish"
+  echo ""
+done
+
+echo "$PRE_LOG job done"
