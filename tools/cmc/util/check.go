39aa438ca16cb39d6b5c60dcdddb40fba4868cd6
wanghongda <harryhdwang@tencent.com>
2022-01-04 17:15:48 +0800

    fix CheckProposalRequestResp

--- tools/cmc/util/check.go
+++ tools/cmc/util/check.go
@@ -14,10 +14,13 @@ import (
 
 func CheckProposalRequestResp(resp *common.TxResponse, needContractResult bool) error {
 	if resp.Code != common.TxStatusCode_SUCCESS {
-		if resp.ContractResult != nil && resp.ContractResult.Code != 0 {
-			return errors.New(resp.ContractResult.Message)
+		if resp.Message == "" {
+			if resp.ContractResult != nil && resp.ContractResult.Code != 0 && resp.ContractResult.Message != "" {
+				return errors.New(resp.ContractResult.Message)
+			}
+			return errors.New(resp.Code.String())
 		}
-		return errors.New(resp.Code.String())
+		return errors.New(resp.Message)
 	}
 
 	if needContractResult && resp.ContractResult == nil {
