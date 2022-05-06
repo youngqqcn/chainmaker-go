48ad27d7b5a1239a9ada2a19bfb34163989d4a20
wanghongda <harryhdwang@tencent.com>
2022-05-06 10:40:04 +0800

    fix https://git.chainmaker.org.cn/chainmaker/issue/-/issues/105

--- tools/cmc/client/contract_user.go
+++ tools/cmc/client/contract_user.go
@@ -19,7 +19,6 @@ import (
 	"chainmaker.org/chainmaker-go/tools/cmc/util"
 	"chainmaker.org/chainmaker/common/v2/crypto"
 	"chainmaker.org/chainmaker/pb-go/v2/common"
-	"chainmaker.org/chainmaker/protocol/v2"
 	sdk "chainmaker.org/chainmaker/sdk-go/v2"
 	sdkutils "chainmaker.org/chainmaker/sdk-go/v2/utils"
 	ethabi "github.com/ethereum/go-ethereum/accounts/abi"
@@ -724,6 +723,11 @@ func freezeOrUnfreezeOrRevokeUserContract(which int) error {
 		payload        *common.Payload
 		whichOperation string
 	)
+
+	if !ethcmn.IsHexAddress(contractName) {
+		contractName = util.CalcEvmContractName(contractName)
+	}
+	fmt.Printf("EVM contract name in hex: %s\n", contractName)
 
 	switch which {
 	case 1:
